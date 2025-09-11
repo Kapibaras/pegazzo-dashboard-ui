252d5a4039e0656117ff9c30058986d6513ca526
IzuOvando <voja210394@gmail.com>
2025-09-11 17:58:45 -0600

    feat: :sparkles: [UI] Setup MTH APIClient

---
+++ src/api/client.ts
@@ -0,0 +1,68 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { APIError, APIRequestFailed } from "./index";
+import CONFIG from "../config";
+
+class APIClient {
+  private static instance: APIClient;
+  private axiosInstance: AxiosInstance;
+
+  private constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: CONFIG.BASE_URL,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
+    });
+    this.axiosInstance.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (axios.isCancel(error) || error.code === "ERR_CANCELED") {
+            console.debug("[API] request canceled:", error.message);
+            return undefined;
+        }
+        if (error.response) {
+            const status = error.response.status;
+            const message =
+              error.response.data?.message ||
+              error.response.statusText ||
+              "Server error";
+            return Promise.reject(new APIError(message, status));
+        }
+        return Promise.reject(
+            new APIRequestFailed(error.message || "Request failed", 500)
+        );   
+      },
+    );
+  }
+
+  public static getInstance(): APIClient {
+    if (!APIClient.instance) {
+      APIClient.instance = new APIClient();
+    }
+    return APIClient.instance;
+  }
+
+  public async get<T = any>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.axiosInstance.get<T>(url, config);
+  }
+
+  public async post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.axiosInstance.post<T>(url, data, config);
+  }
+
+  public async put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.axiosInstance.put<T>(url, data, config);
+  }
+
+  public async patch<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.axiosInstance.patch<T>(url, data, config);
+  }
+
+  public async delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.axiosInstance.delete<T>(url, config);
+  }
+}
+
+export const apiClient = APIClient.getInstance();
---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+import { apiClient } from "./client";
+import { APIError } from "./errors";
+import { APIRequestFailed } from "./errors";
+
+export {
+    apiClient,
+    APIError,
+    APIRequestFailed,
+};
