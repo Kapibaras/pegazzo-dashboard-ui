ed191934c27a503489d0e006cc0fe7d0cdc4de91
IzuOvando <voja210394@gmail.com>
2025-10-22 00:02:56 -0600

    feat: :sparkles: [UI] User Management Create User

--- src/app/(dashboard)/usuarios/page.tsx
+++ src/app/(dashboard)/usuarios/page.tsx
@@ -1,9 +1,19 @@
 import { Container } from '@/components/common';
+import { Input } from '@/components/ui/input';
+import { CreateUserSheet } from '@/components/users/CreateUserSheet';
 
 export default async function UsersPage() {
   return (
     <Container>
-      <h1 className="typo-title">Bienvenidos a Usuarios</h1>
+      <div className="flex items-center gap-3">
+        <Input
+          id="Buscar usuario..."
+          name="Buscar usuario..."
+          placeholder="Buscar usuario..."
+          className="typo-text border-surface-700 bg-surface-400 flex max-w-md items-center self-stretch rounded-md border px-2.5 py-5.5 shadow-sm placeholder:text-left focus:border-blue-600 focus:outline-none"
+        />
+        <CreateUserSheet />
+      </div>
     </Container>
   );
 }
