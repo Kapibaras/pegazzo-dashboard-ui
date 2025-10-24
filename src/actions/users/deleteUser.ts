b7985d5ed6076ddabd7b8cea018f4112409ecc9e
Ruben35 <ruben111416@gmail.com>
2025-10-24 17:19:27 -0600

    feat: :sparkles: Adding delete user action

---
+++ src/actions/users/deleteUser.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+import { ScopedAPIClient } from '@/api';
+import { UserService } from '@/services';
+import { getCookiesServer } from '@/utils/cookies/server';
+
+export async function deleteUserAction(username: string) {
+  if (!username) {
+    return { success: false, message: 'Datos incompletos.' };
+  }
+
+  try {
+    const cookies = await getCookiesServer();
+    const client = new ScopedAPIClient(cookies);
+    const userService = new UserService(client);
+
+    await userService.deleteUser(username);
+
+    revalidatePath('/dashboard/users');
+
+    return { success: true };
+  } catch (error: unknown) {
+    console.error('Error deleting user:', error);
+    const message = error instanceof Error ? error.message : 'Error eliminando el usuario.';
+    return {
+      success: false,
+      message: message,
+    };
+  }
+}
