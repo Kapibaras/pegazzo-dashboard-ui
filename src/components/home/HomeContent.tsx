2ff96319ce5e25db3bdc2236245efbf3410dd996
Ruben35 <ruben111416@gmail.com>
2026-05-19 19:22:21 -0600

    feat: :sparkles: Add Home Cards

--- src/components/home/HomeContent.tsx
+++ src/components/home/HomeContent.tsx
@@ -2,6 +2,7 @@
 
 import { useUser } from '@/contexts/UserProvider';
 import { Role } from '@/lib/schemas/userSchema';
+import HomeAdminDashboard from './HomeAdminDashboard';
 import HomeDashboard from './HomeDashboard';
 import HomeWelcome from './HomeWelcome';
 
@@ -10,6 +11,10 @@ const HomeContent = () => {
 
   if (user.role === Role.OWNER) {
     return <HomeDashboard />;
+  }
+
+  if (user.role === Role.ADMIN) {
+    return <HomeAdminDashboard />;
   }
 
   return <HomeWelcome />;
